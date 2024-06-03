4c3874f974105b7259d3c26339ed475a19e10dcb
xark-10 <84316716+xark-10@users.noreply.github.com>
2024-06-03 14:10:36 +0530

    clean up

--- index.js
+++ index.js
@@ -6,25 +6,12 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const teamRoutes = require('./routes/teamRoutes');
 
-// Endpoints
 
-// app.get('/', async (req, res) => {
-//   res.send('Hello World!');
-// });
 
-// app.get('/demo', async (req, res) => {
-//   await sampleCreate();
-//   res.send({status: 1, message: "demo"});
-// });
-
-// //
-
-// Connect Database
 connectDB();
 
 app.use(bodyParser.json());
 
-// Define Routes
 app.use('/api', teamRoutes);
 
 
--- models/Player.js
+++ models/Player.js
@@ -1,4 +1,3 @@
-// models/Player.js
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
